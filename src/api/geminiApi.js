cd8758d664e5bfb9722c5da3f8a075513133faa4
MAbdullahZahid <mabdulllahzahid@gmail.com>
2025-08-19 15:56:33 +0500

    Context Refer

--- src/api/geminiApi.js
+++ src/api/geminiApi.js
@@ -1,22 +1,16 @@
-//api/geminiApi.js
-
 // src/api/geminiApi.js
-import app from "../firebase"; 
+import app from "../firebase";
 import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
-
-// Initialize the Gemini Developer API backend service
 const ai = getAI(app, { backend: new GoogleAIBackend() });
-
-// Create a `GenerativeModel` instance with a model that supports your use case
 const model = getGenerativeModel(ai, { model: "gemini-2.5-flash" });
+const chatSession = model.startChat({
+
+});
 
 export async function runGemini(prompt) {
   try {
-    // To generate text output, call generateContent with the text input
-    const result = await model.generateContent(prompt);
-
-    const response = result.response;
-    return response.text();
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text();
   } catch (error) {
     console.error("Gemini API Error:", error);
     return "Something went wrong!";
